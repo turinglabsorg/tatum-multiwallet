01e619956ebbef58c1da9396a0b71062281b3be4
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-21 17:43:13 +0100

    min fix

--- index.js
+++ index.js
@@ -8,47 +8,56 @@ async function generate(testnet = false) {
     if(testnet){
         scrypta.testnet = true
     }
-    let mnemonic = await scrypta.generateMnemonic('latin')
-    let seed = await bip39.mnemonicToSeed(mnemonic)
-    let wallet = await scrypta.returnXKeysFromSeed(seed)
-    wallet.mnemonic = mnemonic
-    console.log('GENERATED WALLET', wallet)
-    /*
-        // TEST GENERATING 3 DIFFERENT MNEMONICS
-        const btcWallet = await Tatum.generateWallet(Tatum.Currency.BTC, testnet);
-        const lyraWallet = await Tatum.generateWallet(Tatum.Currency.LYRA, testnet);
-        const ethWallet = await Tatum.generateWallet(Tatum.Currency.ETH, testnet);
-        console.log('BTC WALLET', btcWallet)
-        console.log('LYRA WALLET', lyraWallet)
-        console.log('ETH WALLET', ethWallet)
-    */
+    const mnemonic = "celo spumam custodi fugent metuo pudicitiam nota crystallum martius bigas motricium quia dato senior inventa evolvo ferro auca magna accusatis terricula iudicatus otiosum perfectus"
 
-    /*
-        // GENERATE PUBLIC ADDRESS
-        const btcAddress = Tatum.generateAddressFromXPub(Tatum.Currency.BTC, testnet, wallet.xpub, 0);
-        const lyraAddress = Tatum.generateAddressFromXPub(Tatum.Currency.LYRA, testnet, wallet.xpub, 0);
-        const ethAddress = Tatum.generateAddressFromXPub(Tatum.Currency.ETH, testnet, wallet.xpub, 0);
-    */
-
-    try{
+    /*try{
         const btcPrivKey = await Tatum.generatePrivateKeyFromMnemonic(Tatum.Currency.BTC, testnet, mnemonic, 0);
         console.log('BTC PRIVKEY', btcPrivKey)
         const btcAddress = await Tatum.generateAddressFromPrivatekey(Tatum.Currency.BTC, testnet, btcPrivKey)    
         console.log('BTC ADDRESS', btcAddress)
     }catch(e){
         console.log('ERROR WHILE CREATING BTC ADDRESS')
-    }
+    }*/
     
     try{
         const lyraPrivKey = await Tatum.generatePrivateKeyFromMnemonic(Tatum.Currency.LYRA, testnet, mnemonic, 0);
         console.log('LYRA PRIVKEY', lyraPrivKey)
         const lyraAddress = await Tatum.generateAddressFromPrivatekey(Tatum.Currency.LYRA, testnet, lyraPrivKey)    
         console.log('LYRA ADDRESS', lyraAddress)
+        
+        // FETCHING UNSPENTS
+        const unspent = await Tatum.scryptaGetUnspentForAccount(lyraAddress)
+        let balance = 0
+        for(let k in unspent){
+            balance += unspent[k].amount
+        }
+        console.log('BALANCE IS ' + balance + ' LYRA')
+        const value = balance - 0.001
+        const tx = {
+            fromAddress: [{
+                address: lyraAddress,
+                privateKey: lyraPrivKey
+            }],
+            to: [{
+                    address: lyraAddress,
+                    value: value
+            }]
+        }
+        console.log(tx)
+        
+        const rawtransaction = await Tatum.prepareScryptaSignedTransaction(false, tx);
+        console.log(rawtransaction)
+
+        /*const txid = await Tatum.scryptaBroadcast(rawtransaction)
+        console.log(txid)*/
+        const txid = await scrypta.sendRawTransaction(rawtransaction)
+        console.log(txid)
     }catch(e){
-        console.log('ERROR WHILE CREATING LYRA ADDRESS')
+        console.log(e)
+        console.log('ERROR WHILE PROCESSING LYRA')
     }
 
-    try{
+    /*try{
         const ethPrivKey = await Tatum.generatePrivateKeyFromMnemonic(Tatum.Currency.ETH, testnet, mnemonic, 0);
         console.log('ETH PRIVKEY', ethPrivKey)
         const ethAddress = await Tatum.generateAddressFromPrivatekey(Tatum.Currency.ETH, testnet, ethPrivKey)    
@@ -56,7 +65,7 @@ async function generate(testnet = false) {
     }catch(e){
         console.log(e)
         console.log('ERROR WHILE CREATING ETH ADDRESS')
-    }
+    }*/
     
 }
 
